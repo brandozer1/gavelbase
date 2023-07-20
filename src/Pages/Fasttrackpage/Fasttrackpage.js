550e6dbc2d8fa91d7619257a85b1cf1fb067697c
brandozer1 <brandongamez20oo@gmail.com>
2023-07-20 02:41:53 -0400

    DEBUG SANITY CHECK

--- src/Pages/Fasttrackpage/Fasttrackpage.js
+++ src/Pages/Fasttrackpage/Fasttrackpage.js
@@ -223,6 +223,7 @@ export default function Fasttrackpage() {
     axios.post('https://gavelbaseserver.herokuapp.com/api/addLotImage/', {images: images}).then((res)=>{
       axios.post('https://gavelbaseserver.herokuapp.com/api/appendLot', [
         lotId,
+        '',
         JSON.stringify(finalImages.concat(res.data)),
         upc,
         (itemCount > 1? itemCount+' ' : '') + condition + ' ' + title+ ' | ' + status + (missing.length > 0 ? ' | MISSING ITEMS SEE DESCRIPTION' : ''),
