6f97a6667186b9bb8953041f5bb09468add108f7
brandozer1 <brandongamez20oo@gmail.com>
2023-08-04 19:42:57 -0400

    Update Fasttrackpage.js

--- src/Pages/Fasttrackpage/Fasttrackpage.js
+++ src/Pages/Fasttrackpage/Fasttrackpage.js
@@ -299,8 +299,7 @@ export default function Fasttrackpage() {
     if (locationInput.split('-')[0] == 'loc' || locationInput.split('-')[0] == 'Loc') {
       setLocationInput(locationInput.replace('Loc', 'loc'))
       setLocationCode(locationInput.replace('loc-', ''));
-      let newLoc = locationCode.toUpperCase();
-      setLocationCode(newLoc);
+      setLocationCode(locationCode);
     }
     if (locationInput.split('-')[0] == 'lot' || locationInput.split('-')[0] == 'Lot') {
       setLocationInput(locationInput)
