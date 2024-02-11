1a68740f8b9b178267539d570bdfefaaa50cdc2b
brandozer1 <brandongamez20oo@gmail.com>
2024-02-10 20:23:09 -0500

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -278,7 +278,6 @@ export default function Home() {
             />
           </div>
         </div>
-
         {/* Logo cloud */}
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
